24f275940cc823df245134bee4f98a45193a8bce
Dominique Börner <dominiquebr46@gmail.de>
2023-03-05 20:35:54 +0100

    add 1-oop-car example

---
+++ 1-oop-car-example.py
@@ -0,0 +1,13 @@
+class Car:
+    color = "red";
+
+    def drive(self):
+        print("brumm brumm!");
+
+    def get_color(self):
+        print(self.color);
+
+
+car = Car();
+car.drive();
+car.get_color();
