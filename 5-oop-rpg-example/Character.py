54954695727cf54867e1f6278aadb9fcc3a76f7f
Dominique Börner <dominiquebr46@gmail.de>
2023-03-06 18:54:17 +0100

    add 5-oop-rpg-example

--- 5-oop-rpg-example/Character.py
+++ 5-oop-rpg-example/Character.py
@@ -5,4 +5,15 @@ class Character:
     level = 1;
 
     def attack(self):
-        print(f"i attack for {self.base_attack_damage} damage!");
+        print(f"I attack for {self.base_attack_damage} damage!");
+
+class Yuumi(Character):
+    hp = 200;
+    base_attack_damage = 10;
+
+    def attack(self):
+        return super().attack();
+
+
+yuumi = Yuumi();
+yuumi.attack();
